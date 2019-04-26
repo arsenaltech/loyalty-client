3cfcb6c207c95e0920d00c98569584aa1ade47f7
Shabbir Bharmal <shabbir.bharmal@gmail.com>
2019-04-26 13:40:54 -0400

    unknown change

---
+++ src/Refund.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shabbir
+ * Date: 3/7/19
+ * Time: 10:56 PM
+ */
+
+namespace Arsenaltech\Loyalty;
+
+
+class Refund extends ApiResource
+{
+    const OBJECT_NAME = "refund";
+    use ApiOperations\Create;
+
+
+
+}
